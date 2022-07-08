475b2615d26b579efed83fc4bee7a56128482bb5
FaridCG343 <71570619+FaridCG343@users.noreply.github.com>
2022-07-08 08:15:21 -0600

    Update AndXorBinarios.cpp

--- AndXorBinarios.cpp
+++ AndXorBinarios.cpp
@@ -3,16 +3,36 @@
 #include <math.h>
 using namespace std;
 
-int main(){
-signed long long int numero;
-int numero2 , comp,aux;
-cin>>comp;
-while(comp--){
-numero = 0; numero2 = 1;
-cin>>numero;
-while(!((numero&numero2)>0 && (numero^numero2)>0)){
-numero2++;
-}
-cout<<numero2<<endl;
-}
+int main()
+{
+    int numero;
+    int numero2, cp, aux;
+    cin >> cp;
+    while (cp--)
+    {
+        cin >> numero;
+        if (numero == 1)
+        {
+            cout << "3" << endl;
+        }
+        else if ((numero % 2) == 1)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            bitset<32> n(numero);
+            if (n._Find_next(n._Find_first()) == 32)
+            {
+                cout << numero + 1<<endl;
+            }
+            else
+            {
+                aux = n._Find_first();
+                n.reset();
+                n.set(aux);
+                cout << n.to_ulong() << endl;
+            }
+        }
+    }
 }
